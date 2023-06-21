7a066bc16617eec89d0030486f485f4334f352a9
Yayan faturrohman <107906629+Yayanfr20@users.noreply.github.com>
2023-06-22 01:22:04 +0700

    Update Doa.jsx

--- src/pages/Doa.jsx
+++ src/pages/Doa.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navigation from "../components/Navigation"
 import Footer from "../components/Footer"
 import DoaList from '../components/DoaList'
 const Doa = () => {
+    useEffect(() => {
+        document.title = "QuranApp | Kumpulan Doa - doa";
+    }, [])
     return (
         <div>
             <header>
